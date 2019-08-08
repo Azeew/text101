29b441fd9a5c1407d5f923211c781f26a73d14ac
Azeew <47764924+Azeew@users.noreply.github.com>
2019-08-08 19:51:14 -0300

    Add files via upload

---
+++ AdventureGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+    [SerializeField] State currentState;
+
+	void Start () {
+        currentState = startingState;
+        textComponent.text = currentState.storyText;
+        Debug.Log(currentState.nextStates[0]);
+    }
+	
+	void Update () {
+        ManageStates();
+
+    }
+
+    private void ManageStates()
+    {        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            currentState = currentState.nextStates[0];
+        }
+    }
+
+
+
+}
